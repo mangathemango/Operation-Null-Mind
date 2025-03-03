820b0eb14a5483058eb02d3ee3d7d28262788985
Tonyisstudying <manhcuongnguyen2726@gmail.com>
2025-03-04 00:43:46 +0800

    Temp commit

--- include/Game/player.h
+++ include/Game/player.h
@@ -1,7 +1,7 @@
 #ifndef PLAYER_H
 #define PLAYER_H
 #include <vec2.h>
-
+#include <SDL.h>
 typedef struct {
     Vec2 position;
 } PlayerState;
@@ -17,6 +17,12 @@ typedef struct {
     PlayerConfig config;
 } PlayerData;
 
+typedef struct {
+    SDL_FRect position;
+    SDL_Rect hitbox;
+    float move_speed;
+} Player;
+
 extern PlayerData player;
 
 
@@ -28,5 +34,12 @@ int Player_PostUpdate();
 
 // Actions
 int Player_Move(Vec2 direction);
+void Update_Position();
+void Handle_Collision();
+
+void Init_player(Player* player);
+void Handle_input(Player* player, const Uint8* keystate);
+void Update_player(Player* player);
+void Render_player(Player* player, SDL_Renderer* renderer);
 
 #endif
