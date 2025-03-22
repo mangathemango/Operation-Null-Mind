10d7397edc3e6c5e3c0f388f3ca8045ebf8b045b
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-03-22 15:39:08 +0800

    Doxygen image test

--- include/Core/camera.h
+++ include/Core/camera.h
@@ -1,26 +1,19 @@
-/*
-*   @file camera.h
-?   This file contains the declarations for the camera system.
-?   The camera system is used to convert between world and screen coordinates.
-
-?   For most objects, the variable "position" is in world coordinates.
-?   That's why, the Camera_WorldVecToScreen() function is used in render functions to convert that position to screen coordinates.
-
-?   For example: in src/Game/Player/Base/player_render.c
-?      Animation_Render(
-?          player.resources.animation,            
-*          Camera_WorldVecToScreen(
-*              Vec2_Subtract (
-*                  player.state.position,
-*                  Vec2_Divide(player.animData.spriteSize, 2)
-*              )
-*          ), // This is the position to render the thing
-?          player.animData.spriteSize,
-?          0,
-?          NULL,
-?          flip
-?      );
-*/
+/**
+ * @file camera.h
+ * @brief This file contains the declarations for the camera system.
+ * ![Camera System Overview](camera.png)
+ * The camera system is used to convert between world and screen coordinates.
+ * For most objects, the variable "position" is in world coordinates.
+ * That's why, the Camera_WorldVecToScreen() function is used in render functions to convert that position to screen coordinates.
+ * @author Mango
+ * @date 2025-03-12
+ * 
+ * @section usage Usage Examples
+ * 
+ * ```c
+ * player.state.position
+ * ```
+ */
 
 #pragma once
 
