31f1e698fa40c9df0abed6cec1a262dc96669084
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-04-05 03:15:02 +0800

    Play music at start thingy

--- src/Game/Menu/menu_layout.c
+++ src/Game/Menu/menu_layout.c
@@ -59,7 +59,10 @@ void Menu_Update() {
 
     if (Input_MouseIsOnRect(startButtonRect) && Input->mouse.leftButton.pressed) {
         app.state.currentScene = SCENE_GAME;
-        Sound_Play_Music("Assets/Audio/Music/return0 early level music.wav", 0);
+        if (game.currentStage < 3) Sound_Play_Music("Assets/Audio/Music/return0 early level music.wav", -1);
+        else if (game.currentStage >= 3 && game.currentStage < 7) Sound_Play_Music("Assets/Audio/Music/return0 mid level music.wav", -1);
+        else if (game.currentStage >= 7) Sound_Play_Music("Assets/Audio/Music/return0 late level music.wav", -1);
+        else if (game.currentStage >= 10) Sound_Play_Music("Assets/Audio/Music/return0 boss music.wav", -1);
     }
 
     if (Input_MouseIsOnRect(exitButtonRect)) {
