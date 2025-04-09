d8068b481e7fccaac4171553bf1ca8c82e2c03a0
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-04-10 00:32:55 +0800

    aDD LAZER SYSTEM

--- include/Game/Enemy/Types/enemy_libet.h
+++ include/Game/Enemy/Types/enemy_libet.h
@@ -4,18 +4,27 @@
 #include <enemy.h>
 
 typedef enum {
+    LIBET_FLOATING,
     LIBET_VINCIBLE,
     LIBET_DIAGONAL_LAZER_CHARGING,
     LIBET_DIAGONAL_LAZER_FIRING,
-    
+    LIBET_EXPLOSION_FIRING,
+    LIBET_ROOM_LAZER_FIRING,
+    LIBET_BULLET_HELL_FIRING,
+    LIBET_BIG_LAZER_CHARGING,
+    LIBET_BIG_LAZER_FIRING,
+    LIBET_JUGGERNAUT_SPAMMING,
+    LIBET_SPINNING,
 } LibetState;
 
 /**
  * @brief Configuration structure for Libet boss enemy
  */
 typedef struct {
+    LibetState state;           /**< Current state of the Libet enemy */
 
 } LibetConfig;
+
 
 extern LibetConfig LibetConfigData;
 extern EnemyData LibetData;
