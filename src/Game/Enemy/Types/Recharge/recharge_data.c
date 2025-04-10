3c14ecb1b87618b249e74cdae173b1e1bd2b67eb
wiaydotexe <wiaydotexe@gmail.com>
2025-04-10 22:59:51 +0800

    recharge now heals 100hp

--- src/Game/Enemy/Types/Recharge/recharge_data.c
+++ src/Game/Enemy/Types/Recharge/recharge_data.c
@@ -14,7 +14,7 @@ RechargeConfig RechargeConfigData = {
     .rechargeRadius = 120.0f,
     .isRecharging = false,
 
-    .healAmount = 30,
+    .healAmount = 100,
     .rechargeCooldown = 2.0f,
     .rechargeDuration = 1.0f,
     .timer = 0.0f,
