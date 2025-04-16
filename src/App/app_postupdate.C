2afa7f91ae4fbf797eea3d83a3974aa84c2c17db
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-04-16 16:29:30 +0800

    Fix music stopping in pause

--- src/App/app_postupdate.C
+++ src/App/app_postupdate.C
@@ -75,6 +75,13 @@ int App_PostUpdate() {
             }
             break;
         case SCENE_PAUSE:
+            if(game.currentStage < 3) Sound_Queue_Next_Song("Assets/Audio/Music/return0 early level music.wav");
+            else if (game.currentStage >= 3 && game.currentStage < 7)Sound_Queue_Next_Song("Assets/Audio/Music/return0 mid level music.wav");
+            else if (game.currentStage >= 7 && game.currentStage < 10) Sound_Queue_Next_Song("Assets/Audio/Music/return0 late level music.wav");
+            else if (game.currentStage >= 10) Sound_Queue_Next_Song("Assets/Audio/Music/return0 boss music.wav");
+            if (Input->keyboard.keys[SDL_SCANCODE_ESCAPE].pressed) {
+                app.state.currentScene = SCENE_PAUSE;
+            }
             Pause_Update();
             break;
 
