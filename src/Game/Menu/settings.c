5962e4b27da6b655409f290bcaf3cd80366aeb64
mangathemango <163281872+mangathemango@users.noreply.github.com>
2025-04-21 19:21:44 +0800

    Add some variables for settings

--- src/Game/Menu/settings.c
+++ src/Game/Menu/settings.c
@@ -2,6 +2,22 @@
 #include <input.h>
 #include <app.h>
 
+UIElement* settingsTextElement = NULL;
+UIElement* backButtonElement = NULL;
+SDL_Rect backButtonRect = {27, 140, 200, 15};
+
+UIElement* volumeTextElement = NULL;
+
+UIElement* masterVolumeText;
+SDL_Rect masterVolumeSliderRect = {27, 160, 200, 15};
+
+UIElement* sfxVolumeText;
+SDL_Rect sfxVolumeSliderRect = {27, 180, 200, 15};
+
+UIElement* musicVolumeText;
+SDL_Rect musicVolumeSliderRect = {27, 200, 200, 15};
+
+
 void Settings_Start() {
 
 }
@@ -13,5 +29,8 @@ void Settings_Update() {
 }
 
 void Settings_Render() {
-
+    SDL_SetRenderDrawColor(app.resources.renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(app.resources.renderer, &masterVolumeSliderRect);
+    SDL_RenderFillRect(app.resources.renderer, &sfxVolumeSliderRect);
+    SDL_RenderFillRect(app.resources.renderer, &musicVolumeSliderRect);
 }
